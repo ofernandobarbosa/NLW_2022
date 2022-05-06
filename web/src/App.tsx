e872be09d0465fa5dd7bfb0aefd9b4549b9e5b00
ofernandobarbosa <ofernandobarbosa@gmail.com>
2022-05-05 23:10:25 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+// Componente: é uma função que retorna um HTML
+
+import { Widget } from "./components/Widget";
+
+// Nome da função obrigatóriamente começar com letra maiúscula
+export function App(){
+  return  <Widget/>
+}
